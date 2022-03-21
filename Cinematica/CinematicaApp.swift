43479021b866680e73aad404cce0bf20743a6eec
Yaroslav Nosyk <yaroslav.nosyk@justeattakeaway.com>
2022-03-21 12:28:37 +0100

    Initial project

---
+++ Cinematica/CinematicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinematicaApp.swift
+//  Cinematica
+//
+//  Created by Yaroslav Nosyk on 21.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CinematicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinematica/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cinematica
+//
+//  Created by Yaroslav Nosyk on 21.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
